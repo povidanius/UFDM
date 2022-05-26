71acb6869366bc061036afe4c9dbb594acec6d96
Anonymous <>
2022-05-26 10:35:05 +0300

    added error bar calculation to fig11

--- experiments/figure_1_1.py
+++ experiments/figure_1_1.py
@@ -9,11 +9,13 @@ import sys
 sys.path.insert(0, "../")
 from scipy.signal import medfilt as mf
 from kac_independence_measure import KacIndependenceMeasure
+import os
 
 """
 This is code to reproduce right part of Figure 1.
 """
 
+num_experiments = 25
 
 def get_y(proj_x, epsilon, fun_type):
         if fun_type == 'trigonometric':
@@ -33,6 +35,43 @@ def get_y(proj_x, epsilon, fun_type):
             sys.exit(0)
         return y
 
+def get_file_name(fun_type):
+    id = 0
+    while os.path.isfile('./basic_demonstration/fig11_data_{}_{}.npy'.format(fun_type, id)):
+        id = id + 1
+
+    file_name = './basic_demonstration/fig11_data_{}_{}.npy'.format(fun_type, id)
+    return file_name
+
+def produce_plots(fun_type):
+    z_indep = []
+    z_dep = []
+    for id in range(num_experiments):
+            file_name = './basic_demonstration/fig11_data_{}_{}.npy'.format(fun_type, id)
+            if os.path.isfile(file_name):
+                data = np.array(np.load(file_name))
+                z_indep.append(data[0,:])
+                z_dep.append(data[1,:])            
+    
+    z_indep = np.array(z_indep)            
+    z_dep = np.array(z_dep)        
+    if z_indep.shape[0] < 2:
+        return
+    z_indep = z_indep[:,::10]    
+    z_dep = z_dep[:,::10]     
+    print(z_indep.shape)
+    #breakpoint()
+    mean_indep = np.mean(z_indep,axis=0)       
+    std_indep = np.std(z_indep, axis=0)
+    mean_dep = np.mean(z_dep,axis=0)       
+    std_dep = np.std(z_dep, axis=0)    
+    #breakpoint()
+    f1 = plt.figure()   
+    plt.errorbar(10*np.array(range(0,z_indep.shape[1])), mean_indep, yerr=std_indep, fmt='-.')
+    plt.errorbar(x=10*np.array(range(0,z_dep.shape[1])), y=mean_dep, yerr=std_dep, fmt='-.')
+    plt.savefig('./basic_demonstration/figures/1_1/dependence_detection_{}_error_bars.png'.format(fun_type))
+
+
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -43,11 +82,16 @@ if __name__ == "__main__":
     input_proj_dim = 0 
     lr = 0.01
 
+
+
     if len(sys.argv) != 2:
         print("Usage {} fun_type".format(sys.argv[0]))
         sys.exit(0)
     else:
         fun_type = sys.argv[1]    
+
+    #produce_plots(fun_type)        
+    #sys.exit(0)
 
     # simulate inedependent data
     print("Independent")
@@ -77,4 +121,12 @@ if __name__ == "__main__":
     plt.plot(history_dep)  
     plt.savefig('./basic_demonstration/dependence_detection_{}_{}_{}_lr{}_nbatch{}.png'.format(fun_type, dim_x, dim_y, lr, n_batch))
     plt.savefig('./basic_demonstration/figures/1_1/dependence_detection_{}.png'.format(fun_type))
+
+    fname = get_file_name(fun_type)
+    data_arr = np.array([np.array(history_indep), np.array(history_dep)])    
+    np.save(fname, data_arr)
+    produce_plots(fun_type)     
+    
+
+
 
